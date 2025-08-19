3ae51925c0021432941cee8d22a452449910605f
Tiago Henrique <tiagofreitas231@gmail.com>
2025-08-18 21:04:16 -0300

    cc

---
+++ Exercicio.1/welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+           <meta charset="UTF-8">
+           <title>welcome</title>
+    </head>
+    <body>
+
+        Welcome <?php echo $_POST["name"]; ?><br>
+       Your email address is: <?php echo $_POST["email"]; ?>
+
+    </body>
+</html>
